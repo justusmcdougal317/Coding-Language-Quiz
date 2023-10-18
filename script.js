feea12ca95ae159e167401e3ec564ab906c7ea49
justusmcdougal317 <justusmcdougal317@gmail.com>
2023-10-18 06:47:59 -0700

    script.js update

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
     const initialsInput = document.getElementById("initials");
     const submitButton = document.getElementById("submit-results");
     let score = 0;
-    let quizLoaded = false; // Track if the quiz is loaded
-    let currentQuestion = 0; // Track the current question
+    let quizLoaded = false; 
+    let currentQuestion = 0; 
   
     loadQuizButton.addEventListener("click", function () {
-      // Check if the quiz is already loaded
+     
       if (quizLoaded) {
         if (currentQuestion < 10) {
-          // Clear the quiz container
+          
           quizContainer.removeChild(quizContainer.lastChild);
-          // Display the next question
+        
           displayQuestion(questions, currentQuestion);
           currentQuestion++;
         } else {
-          // Quiz finished, change the button text
+          
           loadQuizButton.textContent = "Finish Quiz";
         }
       } else {
-        // Make an HTTP request to the Quiz API
+       
         fetch("https://quizapi.io/api/v1/questions?apiKey=VpYpm0pkFSwY5XI1Abv4aaUWBAGRP5iq9iThDo7q&limit=10")
           .then((response) => response.json())
           .then((data) => {
             if (data.length > 0) {
-              // Display the first question
+             
               displayQuestion(data, currentQuestion);
-              quizResultsContainer.style.display = "block"; // Show the results container
-              quizLoaded = true; // Mark the quiz as loaded
+              quizResultsContainer.style.display = "block"; 
+              quizLoaded = true; 
             } else {
               quizContainer.innerHTML = "<p>No questions available.</p>";
             }
@@ -45,9 +45,9 @@ document.addEventListener("DOMContentLoaded", function () {
       questionDiv.classList.add("question");
       const question = questions[index];
   
-      // Check if answers are available
+      
       if (Array.isArray(question.answers)) {
-        // Create radio buttons for each answer
+        
         questionDiv.innerHTML = `
           <h2>Question ${index + 1}:</h2>
           <p>${question.question}</p>
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
           `).join('')}
         `;
       } else {
-        // Handle case when answers are not available
+        
         questionDiv.innerHTML = `
           <h2>Question ${index + 1}:</h2>
           <p>${question.question}</p>
@@ -65,25 +65,24 @@ document.addEventListener("DOMContentLoaded", function () {
         `;
       }
   
-      // Add navigation buttons to move between questions
+      
       if (index < questions.length - 1) {
         const nextButton = document.createElement("button");
         nextButton.textContent = "Next Question";
         nextButton.addEventListener("click", function () {
           quizContainer.removeChild(quizContainer.lastChild);
-          // Display the next question
+          
           displayQuestion(questions, index + 1);
           currentQuestion++;
         });
         questionDiv.appendChild(nextButton);
       } else {
-        // Last question, show the Submit button
+        
         const submitButton = document.createElement("button");
         submitButton.textContent = "Submit";
         submitButton.addEventListener("click", function () {
           const initials = initialsInput.value;
-          // You can send the score and initials to a server or store it in localStorage, etc.
-          // For now, we'll just update the displayed score.
+          
           totalScoreElement.textContent = score;
         });
         questionDiv.appendChild(submitButton);
