f7906cf86e29a65d19585be6143c4c76584e5461
justusmcdougal317 <justusmcdougal317@gmail.com>
2023-10-18 14:58:48 -0700

    final final version, read me addition

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Coding-Language-Quiz
+Purpose of this website is to enable users to test their knowledge on common coding language questions.
+This will be done by creating a quiz website that generates questions for you to answer until the timer runs out.
+This was achievable with html,script.js,style.css and quizapi.
+Html was used to set the frame work for the type of webpage being created we also used it to link other used files. All buttons being used on webpage their format originates inside the html file also used to decide language being used etc.
+Script.js was used to make the webpage interactive, in the javascript file we place the functions needed to achieve desired goals.
+I used functions to make calls to the api for the quiz questions and answers upon certain buttons being pressed, thisalso used eventlisteners.
+I used countdown functions to start a timer counting down upon starting the quiz. More event listeres where added to start the quiz before the information was fetched.
+Style css was used to dictate background color , font size, also position of the text was centered using text alighn center command.
+Biggest problem encountered is the api used doesnt transmit answer options on its free api, it sends random generated coding questions but not the multiple choice answer options .For future development I need to link more efficent API to resolve this issue.
--- script.js
+++ script.js
@@ -1,93 +1,108 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loadQuizButton = document.getElementById("load-quiz");
-    const quizContainer = document.getElementById("quiz-container");
-    const quizResultsContainer = document.getElementById("quiz-results");
-    const totalScoreElement = document.getElementById("total-score");
-    const initialsInput = document.getElementById("initials");
-    const submitButton = document.getElementById("submit-results");
-    let score = 0;
-    let quizLoaded = false; 
-    let currentQuestion = 0; 
-  
-    loadQuizButton.addEventListener("click", function () {
-     
-      if (quizLoaded) {
-        if (currentQuestion < 10) {
-          
-          quizContainer.removeChild(quizContainer.lastChild);
+  const loadQuizButton = document.getElementById("load-quiz");
+  const quizContainer = document.getElementById("quiz-container");
+  const quizResultsContainer = document.getElementById("quiz-results");
+  const totalScoreElement = document.getElementById("total-score");
+  const initialsInput = document.getElementById("initials");
+  const submitButton = document.getElementById("submit-results");
+  let score = 0;
+  let quizLoaded = false;
+  let currentQuestion = 0;
+  let countdown;
+  let timeRemaining = 300; 
+
+  function startCountdown() {
+    countdown = setInterval(function () {
+      timeRemaining--;
+      if (timeRemaining <= 0) {
+        clearInterval(countdown);
+        alert("Time's up! Your quiz is finished.");
         
-          displayQuestion(questions, currentQuestion);
-          currentQuestion++;
-        } else {
-          
-          loadQuizButton.textContent = "Finish Quiz";
-        }
-      } else {
-       
-        fetch("https://quizapi.io/api/v1/questions?apiKey=VpYpm0pkFSwY5XI1Abv4aaUWBAGRP5iq9iThDo7q&limit=10")
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.length > 0) {
-             
-              displayQuestion(data, currentQuestion);
-              quizResultsContainer.style.display = "block"; 
-              quizLoaded = true; 
-            } else {
-              quizContainer.innerHTML = "<p>No questions available.</p>";
-            }
-            console.log(data);
-          });
       }
-    });
+      updateTimerDisplay();
+    }, 1000); 
+  }
+
   
-    function displayQuestion(questions, index) {
-      const questionDiv = document.createElement("div");
-      questionDiv.classList.add("question");
-      const question = questions[index];
-  
-      
-      if (Array.isArray(question.answers)) {
-        
-        questionDiv.innerHTML = `
-          <h2>Question ${index + 1}:</h2>
-          <p>${question.question}</p>
-          ${question.answers.map((answer, answerIndex) => `
-            <input type="radio" name="q${index + 1}" value="${answer}" id="q${index + 1}-${answerIndex}">
-            <label for="q${index + 1}-${answerIndex}">${answer}</label><br>
-          `).join('')}
-        `;
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+  const timerDisplay = document.getElementById("timer");
+  timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+}
+
+
+
+
+
+  updateTimerDisplay(); 
+  loadQuizButton.addEventListener("click", function () {
+    if (quizLoaded) {
+      if (currentQuestion < 10) {
+        quizContainer.removeChild(quizContainer.lastChild);
+        displayQuestion(questions, currentQuestion);
+        currentQuestion++;
       } else {
-        
-        questionDiv.innerHTML = `
-          <h2>Question ${index + 1}:</h2>
-          <p>${question.question}</p>
-          <p>Answers not available</p>
-        `;
+        loadQuizButton.textContent = "Finish Quiz";
       }
-  
-      
-      if (index < questions.length - 1) {
-        const nextButton = document.createElement("button");
-        nextButton.textContent = "Next Question";
-        nextButton.addEventListener("click", function () {
-          quizContainer.removeChild(quizContainer.lastChild);
-          
-          displayQuestion(questions, index + 1);
-          currentQuestion++;
+    } else {
+      fetch("https://quizapi.io/api/v1/questions?apiKey=VpYpm0pkFSwY5XI1Abv4aaUWBAGRP5iq9iThDo7q&limit=10")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.length > 0) {
+            displayQuestion(data, currentQuestion);
+            quizResultsContainer.style.display = "block";
+            quizLoaded = true;
+            startCountdown(); 
+          } else {
+            quizContainer.innerHTML = "<p>No questions available.</p>";
+          }
         });
-        questionDiv.appendChild(nextButton);
-      } else {
-        
-        const submitButton = document.createElement("button");
-        submitButton.textContent = "Submit";
-        submitButton.addEventListener("click", function () {
-          const initials = initialsInput.value;
-          
-          totalScoreElement.textContent = score;
-        });
-        questionDiv.appendChild(submitButton);
-      }
-  
-      quizContainer.appendChild(questionDiv);
     }
   });
+
+  function displayQuestion(questions, index) {
+    const questionDiv = document.createElement("div");
+    questionDiv.classList.add("question");
+    const question = questions[index];
+
+    if (Array.isArray(question.answers)) {
+      questionDiv.innerHTML = `
+        <h2>Question ${index + 1}:</h2>
+        <p>${question.question}</p>
+        ${question.answers.map((answer, answerIndex) => `
+          <input type="radio" name="q${index + 1}" value="${answer}" id="q${index + 1}-${answerIndex}">
+          <label for="q${index + 1}-${answerIndex}">${answer}</label><br>
+        `).join('')}
+      `;
+    } else {
+      questionDiv.innerHTML = `
+        <h2>Question ${index + 1}:</h2>
+        <p>${question.question}</p>
+        <p>Answers not available</p>
+      `;
+    }
+
+    if (index < questions.length - 1) {
+      const nextButton = document.createElement("button");
+      nextButton.textContent = "Next Question";
+      nextButton.addEventListener("click", function () {
+        quizContainer.removeChild(quizContainer.lastChild);
+        displayQuestion(questions, index + 1);
+        currentQuestion++;
+      });
+      questionDiv.appendChild(nextButton);
+    } else {
+      const submitButton = document.createElement("button");
+      submitButton.textContent = "Submit";
+      submitButton.addEventListener("click", function () {
+        const initials = initialsInput.value;
+        totalScoreElement.textContent = score;
+      });
+      questionDiv.appendChild(submitButton);
+    }
+
+    quizContainer.appendChild(questionDiv);
+  }
+});
